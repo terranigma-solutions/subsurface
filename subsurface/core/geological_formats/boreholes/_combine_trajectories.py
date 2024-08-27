f7dfb8a88a777ee60079365b624d17a0629ffb8b
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-08-27 15:00:39 +0200

    [WIP/BUG] More robust merging of survey
    [TEST] More borehole data test

---
+++ subsurface/core/geological_formats/boreholes/_combine_trajectories.py
@@ -0,0 +1,110 @@
+import enum
+import numpy as np
+import pandas as pd
+
+from .collars import Collars
+from .survey import Survey
+from ...structs import LineSet
+from ...structs.base_structures import UnstructuredData
+
+
+class MergeOptions(enum.Enum):
+    RAISE = enum.auto()
+    INTERSECT = enum.auto()
+
+
+def create_combined_trajectory(collars: Collars, survey: Survey, merge_option: MergeOptions, slice_: slice):
+    collar_df = _create_collar_df(collars, slice_)
+    survey_df_vertex = _create_survey_df(survey)
+
+    if merge_option == MergeOptions.RAISE:
+        raise NotImplementedError("RAISE merge option not implemented")
+        _validate_matching_ids(collar_df, survey_df_vertex)
+    elif merge_option == MergeOptions.INTERSECT:
+        return _Intersect.process_intersection(collar_df, survey_df_vertex, survey)
+    else:
+        raise ValueError("Unsupported merge option")
+
+
+def _create_collar_df(collars, slice_):
+    collar_df = pd.DataFrame(collars.collar_loc.points[slice_], columns=['X', 'Y', 'Z'])
+    collar_df['well_id'] = collars.ids[slice_]
+    return collar_df
+
+
+def _create_survey_df(survey):
+    survey_df_vertex = pd.DataFrame(survey.survey_trajectory.data.vertex, columns=['X', 'Y', 'Z'])
+    vertex_attrs = survey.survey_trajectory.data.points_attributes
+    id_int_vertex = vertex_attrs['well_id']
+    survey_df_vertex['well_id'] = id_int_vertex.map(pd.Series(survey.ids))
+    return survey_df_vertex
+
+
+def _validate_matching_ids(collar_df, survey_df_vertex):
+    missing_from_survey = set(collar_df['well_id']) - set(survey_df_vertex['well_id'])
+    missing_from_collar = set(survey_df_vertex['well_id']) - set(collar_df['well_id'])
+    if missing_from_survey or missing_from_collar:
+        raise ValueError(f"Collars and survey ids do not match. Missing in survey: {missing_from_survey}, Missing in collars: {missing_from_collar}")
+
+
+class _Intersect:
+    """This class is just to create a namespace for the intersection method"""
+    @staticmethod
+    def process_intersection(collar_df: pd.DataFrame, survey_df_vertex: pd.DataFrame, survey: Survey) -> LineSet:
+        combined_df_vertex = pd.merge(
+            left=survey_df_vertex,
+            right=collar_df,
+            how='outer',
+            indicator=True,
+            on='well_id',
+            suffixes=('_collar', '_survey')
+        )
+        combined_df_vertex = combined_df_vertex[combined_df_vertex['_merge'].isin(['left_only', 'both']) ]
+
+        vertex_attrs = survey.survey_trajectory.data.points_attributes
+        if len(combined_df_vertex) != len(vertex_attrs):
+            raise ValueError("Vertex and vertex attributes have different lengths")
+        _Intersect._add_collar_coordinates(combined_df_vertex)
+
+        combined_df_cells = _Intersect._generate_cells(combined_df_vertex, survey)
+        cell_attributes = survey.survey_trajectory.data.cell_attributes
+        if len(combined_df_cells) != len(cell_attributes):
+            raise ValueError("Cells and cell attributes have different lengths")
+
+        return _Intersect._create_line_set(combined_df_vertex, combined_df_cells, survey)
+
+    @staticmethod
+    def _add_collar_coordinates(combined_df_vertex: pd.DataFrame):
+        combined_df_vertex['X_survey'] += combined_df_vertex['X_collar']
+        combined_df_vertex['Y_survey'] += combined_df_vertex['Y_collar']
+        combined_df_vertex['Z_survey'] += combined_df_vertex['Z_collar']
+
+    @staticmethod
+    def _generate_cells(combined_df_vertex: pd.DataFrame, survey: Survey) -> pd.DataFrame:
+        combined_df_cells = []
+        previous_index = 0
+        for e, well_id in enumerate(survey.ids):
+            df_vertex_well = combined_df_vertex[combined_df_vertex['well_id'] == well_id]
+            indices = np.arange(len(df_vertex_well)) + previous_index
+            previous_index += len(df_vertex_well)
+            cells = np.array([indices[:-1], indices[1:]]).T
+            df_cells_well = pd.DataFrame(cells, columns=['cell1', 'cell2'])
+            df_cells_well['well_id'] = well_id
+            df_cells_well['well_id_int'] = e
+            combined_df_cells.append(df_cells_well)
+
+        return pd.concat(combined_df_cells, ignore_index=True)
+
+    @staticmethod
+    def _create_line_set(combined_df_vertex: pd.DataFrame, combined_df_cells: pd.DataFrame, survey: Survey) -> LineSet:
+        vertex_attrs = survey.survey_trajectory.data.points_attributes
+        cell_attributes = survey.survey_trajectory.data.cell_attributes
+
+        combined_trajectory_unstruct = UnstructuredData.from_array(
+            vertex=combined_df_vertex[['X_survey', 'Y_survey', 'Z_survey']].values,
+            cells=combined_df_cells[['cell1', 'cell2']].values,
+            vertex_attr=vertex_attrs,
+            cells_attr=cell_attributes
+        )
+
+        return LineSet(data=combined_trajectory_unstruct, radius=500)
---
+++ tests/test_io/test_lines/test_lines_II.py
@@ -0,0 +1,182 @@
+import dotenv
+import os
+import pandas as pd
+
+from subsurface import UnstructuredData
+from subsurface.core.geological_formats.boreholes.boreholes import BoreholeSet, MergeOptions
+from subsurface.core.geological_formats.boreholes.collars import Collars
+from subsurface.core.geological_formats.boreholes.survey import Survey
+from subsurface.core.reader_helpers.readers_data import GenericReaderFilesHelper
+from subsurface.core.structs.base_structures.base_structures_enum import SpecialCellCase
+from subsurface.core.structs.unstructured_elements import PointSet
+from subsurface.modules.reader.wells.read_borehole_interface import read_collar, read_survey, read_lith
+from subsurface.modules.visualization import to_pyvista_points, pv_plot, to_pyvista_line, init_plotter
+
+dotenv.load_dotenv()
+
+PLOT = True
+
+data_folder = os.getenv("PATH_TO_ASCII_DRILLHOLES")
+
+
+def test_read_collar():
+    reader: GenericReaderFilesHelper = GenericReaderFilesHelper(
+        file_or_buffer=data_folder + "collars.csv",
+        header=0,
+        usecols=[0, 1, 2, 3],
+        columns_map={
+                "HOLE_ID": "id",  # ? Index name is not mapped
+                "X"      : "x",
+                "Y"      : "y",
+                "Z"      : "z"
+        }
+    )
+    df = read_collar(reader)
+
+    # TODO: df to unstruct
+    unstruc: UnstructuredData = UnstructuredData.from_array(
+        vertex=df[["x", "y", "z"]].values,
+        cells=SpecialCellCase.POINTS
+    )
+
+    points = PointSet(data=unstruc)
+
+    collars = Collars(
+        ids=df.index.to_list(),
+        collar_loc=points
+    )
+
+    if PLOT:
+        s = to_pyvista_points(collars.collar_loc)
+        pv_plot([s], image_2d=True)
+
+
+def test_read_survey():
+    reader: GenericReaderFilesHelper = GenericReaderFilesHelper(
+        file_or_buffer=data_folder + "survey.csv",
+        columns_map={
+                'Distance': 'md',
+                'Dip'     : 'dip',
+                'Azimuth' : 'azi'
+        },
+    )
+    df = read_survey(reader)
+
+    survey: Survey = Survey.from_df(df)
+
+    if PLOT and False:
+        s = to_pyvista_line(
+            line_set=survey.survey_trajectory,
+            radius=10,
+            active_scalar="well_id"
+        )
+        pv_plot([s], image_2d=True)
+
+    return survey
+
+
+def test_read_stratigraphy():
+    reader: GenericReaderFilesHelper = GenericReaderFilesHelper(
+        file_or_buffer=data_folder + "geology.csv",
+        columns_map={
+                'HOLE-ID': 'id',
+                'FROM'   : 'top',
+                'TO'     : 'base',
+                'GEOLOGY': 'component lith'
+        }
+    )
+
+    lith: pd.DataFrame = read_lith(reader)
+    survey: Survey = test_read_survey()
+
+    survey.update_survey_with_lith(lith)
+
+    reader_collar: GenericReaderFilesHelper = GenericReaderFilesHelper(
+        file_or_buffer=data_folder + "collars.csv",
+        header=0,
+        usecols=[0, 1, 2, 3],
+        columns_map={
+                "HOLE_ID": "id",  # ? Index name is not mapped
+                "X"      : "x",
+                "Y"      : "y",
+                "Z"      : "z"
+        }
+    )
+    df_collar = read_collar(reader_collar)
+    collar = Collars.from_df(df_collar)
+
+    borehole_set = BoreholeSet(
+        collars=collar,
+        survey=survey,
+        merge_option=MergeOptions.INTERSECT
+    )
+    borehole_set.get_bottom_coords_for_each_lith()
+
+    foo = borehole_set._merge_vertex_data_arrays_to_dataframe()
+    well_id_mapper: dict[str, int] = borehole_set.survey.id_to_well_id
+    # mapp well_id column to well_name
+    foo["well_name"] = foo["well_id"].map(well_id_mapper)
+
+    if PLOT and True:
+        trajectory = borehole_set.combined_trajectory
+        s = to_pyvista_line(
+            line_set=trajectory,
+            active_scalar="lith_ids",
+            radius=40
+        )
+        clim = [0, 8] # Threshold lith_ids
+        s = s.threshold(clim)
+
+        collar_mesh = to_pyvista_points(collar.collar_loc)
+
+        p = init_plotter()
+        import matplotlib.pyplot as plt
+        boring_cmap = plt.get_cmap("viridis", 8)
+        p.add_mesh(s, clim=clim, cmap=boring_cmap)
+        p.add_mesh(collar_mesh, render_points_as_spheres=True)
+        p.add_point_labels(
+            points=collar.collar_loc.points,
+            labels=collar.ids,
+            point_size=10,
+            shape_opacity=0.5,
+            font_size=12,
+            bold=True
+        )
+        p.show()
+
+
+def test_merge_collar_survey():
+    reader_collar: GenericReaderFilesHelper = GenericReaderFilesHelper(
+        file_or_buffer=os.getenv("PATH_TO_SPREMBERG_COLLAR"),
+        header=0,
+        usecols=[0, 1, 2, 4],
+        columns_map={
+                "hole_id"            : "id",  # ? Index name is not mapped
+                "X_GK5_incl_inserted": "x",
+                "Y__incl_inserted"   : "y",
+                "Z_GK"               : "z"
+        }
+    )
+    df_collar = read_collar(reader_collar)
+    collar = Collars.from_df(df_collar)
+
+    reader_survey: GenericReaderFilesHelper = GenericReaderFilesHelper(
+        file_or_buffer=os.getenv("PATH_TO_SPREMBERG_SURVEY"),
+        columns_map={
+                'depth'  : 'md',
+                'dip'    : 'dip',
+                'azimuth': 'azi'
+        },
+    )
+
+    survey = Survey.from_df(read_survey(reader_survey))
+
+    borehole_set = BoreholeSet(
+        collars=collar,
+        survey=survey,
+        merge_option=MergeOptions.INTERSECT
+    )
+
+    if PLOT:
+        s = to_pyvista_line(line_set=borehole_set.combined_trajectory, radius=50)
+        pv_plot([s], image_2d=True)
