eac784d8c132e1aa162f15e7c21f109ae965a96e
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-08-28 10:53:05 +0200

    [TEST] Adding some more importing wells tests

--- subsurface/core/geological_formats/boreholes/boreholes.py
+++ subsurface/core/geological_formats/boreholes/boreholes.py
@@ -11,6 +11,36 @@ from ...structs import LineSet
 
 @dataclass
 class BoreholeSet:
+    """
+    This module provides a class, `BoreholeSet`, that represents a collection of boreholes. It contains methods for accessing coordinate data for each lithology in the boreholes.
+    
+    Notes: 
+        - Collars is defined as 1 UnstructuredData
+        - Combined trajectory is defined as 1 UnstructuredData
+
+    Classes:
+        - `BoreholeSet`: Represents a collection of boreholes.
+
+    Methods:
+        - `__init__`: Initializes a new `BoreholeSet` object with the specified input parameters.
+        - `get_top_coords_for_each_lith`: Returns a dictionary of top coordinates for each lithology in the boreholes.
+        - `get_bottom_coords_for_each_lith`: Returns a dictionary of bottom coordinates for each lithology in the boreholes.
+
+    Attributes:
+        - `collars`: A `Collars` object representing the collar information for the boreholes.
+        - `survey`: A `Survey` object representing the survey information for the boreholes.
+        - `combined_trajectory`: A `LineSet` object representing the combined trajectory of the boreholes.
+
+    Usage:
+        ```
+        borehole_set = BoreholeSet(collars, survey, merge_option)
+        top_coords = borehole_set.get_top_coords_for_each_lith()
+        bottom_coords = borehole_set.get_bottom_coords_for_each_lith()
+        ```
+
+    Note: The example usage code provided above is for demonstration purposes only. Please replace `collars`, `survey`, and `merge_option` with the actual input parameters when using the `BoreholeSet` class.
+
+    """
     __slots__ = ['collars', 'survey', 'combined_trajectory']
     collars: Collars
     survey: Survey
--- tests/test_io/test_lines/test_lines_II.py
+++ tests/test_io/test_lines/test_lines_II.py
@@ -20,6 +20,14 @@ data_folder = os.getenv("PATH_TO_ASCII_DRILLHOLES")
 
 
 def test_read_collar():
+    collars = _read_collars()
+
+    if PLOT:
+        s = to_pyvista_points(collars.collar_loc)
+        pv_plot([s], image_2d=True)
+
+
+def _read_collars() -> Collars:
     reader: GenericReaderFilesHelper = GenericReaderFilesHelper(
         file_or_buffer=data_folder + "collars.csv",
         header=0,
@@ -32,23 +40,17 @@ def test_read_collar():
         }
     )
     df = read_collar(reader)
-
     # TODO: df to unstruct
     unstruc: UnstructuredData = UnstructuredData.from_array(
         vertex=df[["x", "y", "z"]].values,
         cells=SpecialCellCase.POINTS
     )
-
     points = PointSet(data=unstruc)
-
     collars = Collars(
         ids=df.index.to_list(),
         collar_loc=points
     )
-
-    if PLOT:
-        s = to_pyvista_points(collars.collar_loc)
-        pv_plot([s], image_2d=True)
+    return collars
 
 
 def test_read_survey():
@@ -119,37 +121,76 @@ def test_read_stratigraphy():
 
     if PLOT and True:
         trajectory = borehole_set.combined_trajectory
-        s = to_pyvista_line(
-            line_set=trajectory,
-            active_scalar="lith_ids",
-            radius=40
-        )
-        clim = [0, 8]  # Threshold lith_ids
-        s = s.threshold(clim)
+        scalar = "lith_ids"
+        _plot(scalar, trajectory, collar)
 
-        collar_mesh = to_pyvista_points(collar.collar_loc)
 
-        p = init_plotter()
-        import matplotlib.pyplot as plt
-        boring_cmap = plt.get_cmap("viridis", 8)
-        p.add_mesh(s, clim=clim, cmap=boring_cmap)
+def _plot(scalar, trajectory, collars = None):
+    s = to_pyvista_line(
+        line_set=trajectory,
+        active_scalar=scalar,
+        radius=40
+    )
+    p = init_plotter()
+    import matplotlib.pyplot as plt
+    boring_cmap = plt.get_cmap("viridis", 8)
+    p.add_mesh(s, cmap=boring_cmap)
+    
+    if collars is not None:
+        collar_mesh = to_pyvista_points(collars.collar_loc)
         p.add_mesh(collar_mesh, render_points_as_spheres=True)
         p.add_point_labels(
-            points=collar.collar_loc.points,
-            labels=collar.ids,
+            points=collars.collar_loc.points,
+            labels=collars.ids,
             point_size=10,
             shape_opacity=0.5,
             font_size=12,
             bold=True
         )
-        p.show()
-
-
-def test_read_attr_only_depth():
-    pass
+    p.show()
 
 
 def test_read_attr():
+    survey = _read_geochem_into_survey()
+
+    if PLOT and True:
+        s = to_pyvista_line(
+            line_set=survey.survey_trajectory,
+            radius=10,
+            active_scalar="Ag"
+        )
+        pv_plot([s], image_2d=False)
+
+
+        s = to_pyvista_line(
+            line_set=survey.survey_trajectory,
+            radius=10,
+            active_scalar="MnO"
+        )
+        pv_plot([s], image_2d=False)
+
+    pass
+
+
+def test_read_attr_into_borehole():
+    collars = _read_collars()
+    survey = _read_geochem_into_survey()
+
+    borehole_set = BoreholeSet(
+        collars=collars,
+        survey=survey,
+        merge_option=MergeOptions.INTERSECT
+    )
+
+    _plot(
+        scalar="MnO",
+        trajectory=borehole_set.combined_trajectory,
+        collars=collars
+    )
+
+
+
+def _read_geochem_into_survey() -> Survey:
     reader: GenericReaderFilesHelper = GenericReaderFilesHelper(
         file_or_buffer=data_folder + "geochem.csv",
         columns_map={
@@ -158,9 +199,7 @@ def test_read_attr():
                 'to'    : 'base',
         }
     )
-
     attributes: pd.DataFrame = read_attributes(reader)
-
     reader: GenericReaderFilesHelper = GenericReaderFilesHelper(
         file_or_buffer=data_folder + "survey.csv",
         columns_map={
@@ -170,8 +209,7 @@ def test_read_attr():
         },
     )
     df = read_survey(reader)
-
     survey: Survey = Survey.from_df(df)
     survey.update_survey_with_attr(attributes)
+    return survey
 
-    pass
