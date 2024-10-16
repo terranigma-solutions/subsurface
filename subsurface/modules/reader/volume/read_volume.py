632758c95c24fdf6826d2eacd57c1f3bb35554df
Miguel <miguel@terranigma-solutions.com>
2024-10-16 11:00:23 +0200

    [ENH] read_VTK structured grid from stream working

--- subsurface/modules/reader/volume/read_volume.py
+++ subsurface/modules/reader/volume/read_volume.py
@@ -1,3 +1,4 @@
+import os
 from io import BytesIO
 from typing import Union
 
@@ -12,10 +13,24 @@ import pandas as pd
 def read_VTK_structured_grid(file_or_buffer: Union[str, BytesIO]) -> StructuredData:
     pv = optional_requirements.require_pyvista()
 
-    reader = pv.get_reader("vtk")
-    pyvista_obj: pv.DataSet = pv.read(file_or_buffer)
-
-
+    if isinstance(file_or_buffer, BytesIO):
+        # If file_or_buffer is a BytesIO, write it to a temporary file
+        from tempfile import NamedTemporaryFile
+        with NamedTemporaryFile('wb', suffix='.vtk', delete=False) as temp_file:
+            # Write the BytesIO content to the temporary file
+            getvalue: bytes = file_or_buffer.getvalue()
+            temp_file.write(getvalue)
+            temp_file.flush()  # Make sure all data is written
+            temp_file_name = temp_file.name  # Store the temporary file name
+        try:
+            # Use pyvista.read() to read from the temporary file
+            pyvista_obj = pv.read(temp_file_name)
+        finally:
+            # Ensure the temporary file is deleted after reading
+            os.remove(temp_file_name)
+    else:
+        # If it's a file path, read directly
+        pyvista_obj = pv.read(file_or_buffer)
     try:
         pyvista_struct: pv.ExplicitStructuredGrid = pyvista_obj.cast_to_explicit_structured_grid()
     except Exception as e:
